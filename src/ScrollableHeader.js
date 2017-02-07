7ce5210fa95e244f5a664fd703b1318b8ef0a855
Ahmed Hassan <a.hassan@buismed.de>
2017-02-07 23:23:23 +0100

    working header scroll

---
+++ src/ScrollableHeader.js
@@ -0,0 +1,171 @@
+import React, {Component} from 'react';
+import {
+  Animated,
+  Image,
+  Platform,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const HEADER_MAX_HEIGHT = 200;
+const HEADER_MIN_HEIGHT = Platform.OS === 'ios' ? 60 : 73;
+const HEADER_SCROLL_DISTANCE = 30;
+
+export default class ScrollableHeader extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      scrollY: new Animated.Value(0),
+    };
+  }
+
+  _renderScrollViewContent() {
+    const data = Array.from({length: 30});
+
+     const headerMargin = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE],
+      outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT + HEADER_SCROLL_DISTANCE],
+      extrapolate: 'clamp',
+    });
+     
+    return (
+      <Animated.View style={{marginTop: headerMargin}}>
+        {data.map((_, i) =>
+          <View key={i} style={styles.row}>
+            <Text>{i}</Text>
+          </View>
+        )}
+      </Animated.View>
+    );
+  }
+
+  render() {
+    const headerHeight = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE],
+      outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+      extrapolate: 'clamp',
+    });
+
+    const imageOpacity = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+      outputRange: [1, 1, 0],
+      extrapolate: 'clamp',
+    });
+    const imageTranslate = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE],
+      outputRange: [0, -50],
+      extrapolate: 'clamp',
+    });
+
+    const titleScale = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+      outputRange: [1, 1, 0.8],
+      extrapolate: 'clamp',
+    });
+    const titleTranslate = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+      outputRange: [0, 0, -8],
+      extrapolate: 'clamp',
+    });
+
+    let _scrollView;
+    let hasMomentum = false;
+
+    const scrollToSnapPoints = (event) => {
+
+      const currentY = event.nativeEvent.contentOffset.y;
+      console.log(currentY);
+      if(currentY < HEADER_SCROLL_DISTANCE){
+          if(currentY > (HEADER_SCROLL_DISTANCE * 0.5) ){
+          _scrollView.scrollTo({ y: HEADER_SCROLL_DISTANCE })
+        }else{
+          _scrollView.scrollTo({ y: 0 })
+        }
+      }
+    }
+
+    return (
+      <View style={styles.fill}>
+        <StatusBar
+          translucent
+          barStyle="light-content"
+          backgroundColor="rgba(0, 0, 0, 0.251)"
+        />
+        <ScrollView
+          style={styles.fill}
+          ref={(scrollView) => { _scrollView = scrollView; }}
+          scrollEventThrottle={16}
+          onScroll={Animated.event(
+            [{nativeEvent: {contentOffset: {y: this.state.scrollY}}}]
+          )}
+        >
+          {this._renderScrollViewContent()}
+        </ScrollView>
+        <Animated.View style={[styles.header, {height: headerHeight}]}>
+          <Animated.Image
+            style={[
+              styles.backgroundImage,
+              {opacity: imageOpacity, transform: [{translateY: imageTranslate}]},
+            ]}
+            source={require('./images/cat.jpg')}
+          />
+          <Animated.View
+            style={[
+              styles.bar,
+              {transform: [{scale: titleScale}, {translateY: titleTranslate}]},
+            ]}
+          >
+            <Text style={styles.title}>Title</Text>
+          </Animated.View>
+        </Animated.View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  fill: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+  },
+  header: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: '#03A9F4',
+    overflow: 'hidden',
+  },
+  backgroundImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    width: null,
+    height: HEADER_MAX_HEIGHT,
+    resizeMode: 'cover',
+  },
+  bar: {
+    marginTop: Platform.OS === 'ios' ? 28 : 38,
+    height: 32,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: 'white',
+    fontSize: 18,
+  },
+  row: {
+    height: 40,
+    margin: 16,
+    backgroundColor: '#D3D3D3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
